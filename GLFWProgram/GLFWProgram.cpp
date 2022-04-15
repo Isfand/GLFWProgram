71ebc8b237154e12d10896dcbb565b2710679786
Isfand <Isfand@hotmail.co.uk>
2022-04-16 00:17:52 +0100

    Updated

--- GLFWProgram/GLFWProgram.cpp
+++ GLFWProgram/GLFWProgram.cpp
@@ -10,20 +10,11 @@ int main(void)
     GLFWwindow* window;
     if (!glfwInit())
         exit(EXIT_FAILURE);
-
-    // cout << "default shader lang: " << glGetString(GL_SHADING_LANGUAGE_VERSION) << endl;
-
-    // select opengl version
-    // int major, minor, rev;
-    // glfwGetVersion(&major, &minor, &rev);
-    // cout << "glfw major.minor " << major << "." << minor << "." << rev << endl;
     
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-
 
     window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
     if (!window)
