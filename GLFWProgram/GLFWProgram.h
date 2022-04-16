415ec8c2ed69f82c881ab7b6f76fc820f5641656
Isfand <Isfand@hotmail.co.uk>
2022-04-16 18:08:47 +0100

    Updated

--- GLFWProgram/GLFWProgram.h
+++ GLFWProgram/GLFWProgram.h
@@ -3,9 +3,14 @@
 
 #pragma once
 
+#ifndef GLFWPROGRAM_HEADER
+#define GLFWPROGRAM_HEADER
+
 #include <iostream>
 #include "GLFW/glfw3.h"
 #include <stdlib.h>
 #include <stdio.h>
+
+#endif
 
 // TODO: Reference additional headers your program requires here.
